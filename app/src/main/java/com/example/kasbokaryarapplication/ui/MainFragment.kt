c0bcb7be3afacf6605104d617568a6bfca62d7a4
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-07-05 13:58:46 +0430

    feat / add navigation graph to app

---
+++ app/src/main/java/com/example/kasbokaryarapplication/ui/MainFragment.kt
@@ -0,0 +1,8 @@
+package com.example.kasbokaryarapplication.ui
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+}
