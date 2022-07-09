e18cbe904a1c43a4ca1cdf3b0d93d55360bd87d9
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-07-09 16:14:53 +0430

    feat / add binding to first page fragments

--- app/src/main/java/com/example/kasbokaryarapplication/ui/ModernTechFragment.kt
+++ app/src/main/java/com/example/kasbokaryarapplication/ui/ModernTechFragment.kt
@@ -1,8 +1,30 @@
 package com.example.kasbokaryarapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.kasbokaryarapplication.R
+import com.example.kasbokaryarapplication.databinding.FragmentModernTechBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ModernTechFragment : Fragment() {
+
+    lateinit var binding: FragmentModernTechBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_modern_tech, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/example/kasbokaryarapplication/ui/SupportTeamFragment.kt
+++ app/src/main/java/com/example/kasbokaryarapplication/ui/SupportTeamFragment.kt
@@ -1,6 +1,29 @@
 package com.example.kasbokaryarapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.kasbokaryarapplication.R
+import com.example.kasbokaryarapplication.databinding.FragmentSupportTeamBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SupportTeamFragment : Fragment() {
+    lateinit var binding: FragmentSupportTeamBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_support_team, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
