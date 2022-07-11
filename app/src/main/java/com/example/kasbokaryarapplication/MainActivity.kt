30a4838d49aa5e75c6e4b3e23ec07df3574565d5
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-07-11 15:23:41 +0430

    feat / add main bottom menu components to MainActivity.kt

--- app/src/main/java/com/example/kasbokaryarapplication/MainActivity.kt
+++ app/src/main/java/com/example/kasbokaryarapplication/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.kasbokaryarapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kasbokaryarapplication.ui.ContactFragment
+import com.example.kasbokaryarapplication.ui.EducationFragment
+import com.example.kasbokaryarapplication.ui.HomeFragment
+import com.example.kasbokaryarapplication.ui.ServiceFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +15,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigation = findViewById<BottomNavigationView>(R.id.navigation)
+
+        val homeFragment = HomeFragment()
+        val serviceFragment = ServiceFragment()
+        val educationFragment = EducationFragment()
+        val contactFragment = ContactFragment()
+
+        setCurrentFragment(homeFragment)
+
+        navigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.action_main -> setCurrentFragment(homeFragment)
+                R.id.action_services -> setCurrentFragment(serviceFragment)
+                R.id.action_education -> setCurrentFragment(educationFragment)
+                R.id.action_contact -> setCurrentFragment(contactFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment,fragment)
+            commit()
+        }
 }
