acef69acbb24cb1c6cd03cee6a95999f016082eb
YasamanTaleghani <y.taleghani@adpdigital.com>
2022-07-12 17:22:53 +0430

    feat / update ContactFragment.kt

--- app/src/main/java/com/example/kasbokaryarapplication/ui/ContactFragment.kt
+++ app/src/main/java/com/example/kasbokaryarapplication/ui/ContactFragment.kt
@@ -1,8 +1,44 @@
 package com.example.kasbokaryarapplication.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.kasbokaryarapplication.R
+import com.example.kasbokaryarapplication.databinding.FragmentContactBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ContactFragment : Fragment() {
+class ContactFragment : Fragment(), OnMapReadyCallback {
+    lateinit var binding: FragmentContactBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_contact, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val kasbokaryar = LatLng(34.795612,46.92675)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(kasbokaryar)
+                .title("کسب و کار یار")
+        )
+    }
+
+
 }
